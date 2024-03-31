398ef5f8091ce9c1743a38546acd0be0b090ec22
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-03-31 22:46:13 +0530

    Update Quality_Score.py

--- Quality_Score.py
+++ Quality_Score.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import openpyxl
 
 # Function to calculate quality score in percentage
 def calculate_quality_score_percentage(critical_errors, non_critical_errors, sample):
